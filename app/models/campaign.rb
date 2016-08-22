30fe3b35e217435ffa32908b7a3d4ef34d8a4d1c
Charly Martin <charly.martin.1207@gmail.com>
2016-08-22 18:27:19 +0200

    Fixed: validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,8 +5,8 @@ class Campaign < ApplicationRecord
   validates :title, presence: true
   validates :batch_size, presence: true
   validates :date_start, presence: true
-  validates :sdate_end, presence: true
-  validates :sspec, presence: true
-  validates :sprice, presence: true
-  validates :scategory, presence: true
+  validates :date_end, presence: true
+  validates :spec, presence: true
+  validates :price, presence: true
+  validates :category, presence: true
 end
