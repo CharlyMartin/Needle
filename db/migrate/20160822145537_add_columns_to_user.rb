b59ad86019e031a1b86efa4e6211f1e8a5f94d05
Charly Martin <charly.martin.1207@gmail.com>
2016-08-22 18:39:13 +0200

    Modified: binary to gender for gender data in User table

--- db/migrate/20160822145537_add_columns_to_user.rb
+++ db/migrate/20160822145537_add_columns_to_user.rb
@@ -3,7 +3,7 @@ class AddColumnsToUser < ActiveRecord::Migration[5.0]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :address, :string
-    add_column :users, :gender, :binary
+    add_column :users, :gender, :string
     add_column :users, :date_of_birth, :date
   end
 end
