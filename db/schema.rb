ed51031774999d5ddc6762b24d53a1a395d54ccc
amauryjaskowiec <amaury.jaskowiec@gmail.com>
2016-08-23 17:48:47 +0200

    campaigns show + index +navbar link

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160823124740) do
-
+ActiveRecord::Schema.define(version: 20160823134748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
