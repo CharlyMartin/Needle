6d37600e2c0713343d0fc2ce5c633eba327d86d2
Felix Bungener <felixbungener@gmail.com>
2016-08-24 13:47:55 +0200

    now if a campaign is finished you can't order

--- app/controllers/dashboard/campaigns_controller.rb
+++ app/controllers/dashboard/campaigns_controller.rb
@@ -27,6 +27,14 @@ class Dashboard::CampaignsController < ApplicationController
   def update
   end
 
+  # def finished?
+  #   if Date.today.to_time - @campaign.date_end < 0
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+
   private
 
   def campaign_params
