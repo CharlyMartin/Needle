ac0a563894990259086291e960c38bf27b5bb85b
amauryjaskowiec <amaury.jaskowiec@gmail.com>
2016-08-31 17:19:58 +0200

    button follow + hover on cards on user-show + links repaired

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :users, only: [:show, :edit, :update] do
-    post '/followings', to: 'dashboard/followings#create', as: :create_following
+    resources :followings, only: [:create, :destroy], shallow: true
   end
 
   resources :campaigns, only: [:index, :show, :update]
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
       resources :payments, only: [:new, :create]
     end
 
-    resources :followings, only: [:destroy]
+
   end
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
