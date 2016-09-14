55cb4ccda77e7fe590f9cedf0b6ce5716457d971
PierreVincent Taisne <pvtaisne@gmail.com>
2016-09-14 10:27:33 +0200

    admin page

---
+++ app/admin/user.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register User do
+
+    index do
+    selectable_column
+    column :id
+    column :email
+    column :name
+    column :created_at
+    column :admin
+    actions
+  end
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,4 +1,22 @@
+
+def authenticate_admin!
+  redirect_to new_user_session_path unless current_user && current_user.admin
+end
+
+
+
+
 ActiveAdmin.setup do |config|
+
+  # [...]
+  config.authentication_method = :authenticate_admin!
+  # [...]
+  config.current_user_method = :current_user
+  # [...]
+  config.logout_link_path = :destroy_user_session_path
+
+  # [...]
+  config.logout_link_method = :delete
   # == Site Title
   #
   # Set the title that is displayed on the main layout
@@ -98,7 +116,17 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  #
+  #
+  #
+
+
+
+
+
+
+  #
+  # config.logout_link_path = :destroy_admin_user_session_path
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
